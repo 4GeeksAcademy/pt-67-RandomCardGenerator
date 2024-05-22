01e55dabbe49966db7d853ad579a7fbec7e9e9f7
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-05-22 18:52:13 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function(cardCreator) {
+  let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suit = ["diamond", "spade", "heart", "club"];
+  
 };
+
+let randomNumber = number [Math.floor(Math.random() * number.length)];
+number.innerHTML = cardNumber;
+
+let randomSuit = suit [Math.floor(Math.random() * suit.length)];
+suit.innerHTML = cardSuit;
+
